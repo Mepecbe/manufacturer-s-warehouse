f7f4123e62eef0ee40fc3a5fae64a87b8e1e2eb1
Никита <val0dko99@mail.ru>
2020-05-31 00:46:58 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Программное средство учёта бытовой техники на складе производителя
+
+namespace BDiSUBD
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
